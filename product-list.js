c943320eac7aff2b138e462ec06faf9fcaf15fdd
J Manuel HK <jesusmanuelhk@gmail.com>
2020-05-12 12:36:46 -0500

    update

--- product-list.js
+++ product-list.js
@@ -1,3 +1,31 @@
-function loadList() {
-    
+
+var productList = [
+    {
+        "name":"Cubrebocas",
+        "price": 150.00
+    },
+    {
+        "name":"Guantes",
+        "price": 20.00
+    },
+    {
+        "name":"Caretas",
+        "price": 85.00
+    },
+
+]
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', loadList(productList))
+} else {
+    loadList(productList)
+}
+
+function loadList(list) {
+    for (let index = 0; index < list.length; index++) {
+        const element = list[index];
+
+        console.log(element.name);
+        console.log(element.price);
+
+    }
 }
